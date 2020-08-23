9eb996ac06c112ff6eb7374cbd9260bce277e7c7
Michael Albrecht <michaelalbrecht.hays@gmail.com>
2020-08-23 13:40:54 -0500

    Initial Commit

---
+++ OIMA/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  OIMA
+//
+//  Created by Michael Albrecht on 8/23/20.
+//  Copyright © 2020 Michael Albrecht. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
