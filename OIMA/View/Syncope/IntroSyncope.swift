a51ed4ef6a55651ce711c5c513afb543201d439c
Michael Albrecht <michaelalbrecht.hays@gmail.com>
2021-01-03 12:41:49 -0600

    Created enum for syncope sheet to show either hospitalization reasons or references

--- OIMA/View/Syncope/IntroSyncope.swift
+++ OIMA/View/Syncope/IntroSyncope.swift
@@ -8,51 +8,68 @@
 
 import SwiftUI
 
+enum ModalView {
+    case hospitalize
+    case reference
+}
+
 struct IntroSyncope: View {
     
-    @State private var showReferences = false
+    
+    @State private var modalView: ModalView = .hospitalize
+    @State private var isSheetShown = false
     @State private var nextView = false
-    @State private var showHospitalize = false
+
     
     var body: some View {
         ScrollView {
             VStack {
-                Spacer()
+                
                 Text("A quick note").fontWeight(.black).padding().foregroundColor(.red)
 
-                Text("The evaluation of syncope should start with a thorough history and physical exam.  The exam should include cardiac and neurologic examinations. \n\nIn addition, orthostatic vitals (blood pressure and heart rate) should be obtained with patient lying down, sitting, immediately upon standing, and after 3 minutes of standing.\n\nAn ECG should be performed as well.\n\nAfter this initial evaluation, it should be determined whether or not patient should be hospitalized.  Click on the 'Hospitalize?' button to see possible medical conditions that might warrant hospitalization for syncope.")
+                Text("The evaluation of syncope should start with a thorough history and physical exam.  The exam should include cardiac and neurologic examinations. \n\nIn addition, orthostatic vitals (blood pressure and heart rate) should be obtained with patient lying down, sitting, immediately upon standing, and after 3 minutes of standing.\n\nAn ECG should be performed as well.\n\nAfter this initial evaluation, it should be determined whether or not patient should be hospitalized.  Click on the 'Hospitalize?' button to see possible medical conditions that might warrant hospitalization for syncope.\n").padding()
                 
                 Button(action: {
-                    self.showHospitalize.toggle()
+                    self.modalView = .hospitalize
+                    self.isSheetShown.toggle()
                 }) {
                     Text("Hospitalize?")
-                }.sheet(isPresented: $showHospitalize) {
-                    SyncopeHospitalizeReasons()
                 }.buttonStyle(NextButtonStyle(fillColor: red))
-                Spacer()
                 
-                
-                Text("If the patient does not need hospitalization proceed with this outpatient algorithm.")
+                Text("If the patient does not need hospitalization, proceed with this algorithm.").padding(.vertical).padding(.horizontal, 0)
+               
                 Spacer()
                     .frame(height: 50)
+                
                 NextButton1(nextView: $nextView, fillColor: red)
                 NavigationLink(destination: MHReference(), isActive: $nextView) { EmptyView() }
             
-            }.padding()
+            }
             .navigationBarTitle("Quick note", displayMode: .inline)
             .navigationBarItems(trailing: Button(action: {
-                self.showReferences.toggle()
+                self.modalView = .reference
+                self.isSheetShown.toggle()
             }) {
                 Image(systemName: "r.square")
-            }).sheet(isPresented: $showReferences) {
-                SyncopeReference()
+            })
+            .sheet(isPresented: $isSheetShown,  content: {
+                if self.modalView == .hospitalize {
+                    SyncopeHospitalizeReasons()
+                } else if self.modalView == .reference {
+                    SyncopeReference()
+                }
+            })
         }
         }
     }
-}
+
 
 struct IntroSyncope_Previews: PreviewProvider {
     static var previews: some View {
-        IntroSyncope()
+        Group {
+            IntroSyncope()
+            IntroSyncope()
+            IntroSyncope()
+        }
     }
 }
